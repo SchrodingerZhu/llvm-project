7640c14a7f40fd1fd70e5fdbe5435a871866dcbd
Schrodinger ZHU Yifan <i@zhuyi.fan>
2026-09-17 18:53:01 -0400

    [libc][benchmark] lift benchmark options to a separate header library

--- libc/benchmarks/gpu/LibcGpuBenchmark.h
+++ libc/benchmarks/gpu/LibcGpuBenchmark.h
@@ -1,6 +1,21 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+///
+///
+/// \file
+/// GPU benchmark interface and math helpers.
+///
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_LIBC_BENCHMARKS_LIBC_GPU_BENCHMARK_H
 #define LLVM_LIBC_BENCHMARKS_LIBC_GPU_BENCHMARK_H
 
+#include "benchmarks/Benchmark.h"
 #include "benchmarks/gpu/Random.h"
 
 #include "benchmarks/gpu/timing/timing.h"
@@ -17,18 +32,6 @@
 namespace LIBC_NAMESPACE_DECL {
 
 namespace benchmarks {
-
-struct BenchmarkOptions {
-  uint32_t initial_iterations = 1;
-  uint32_t min_iterations = 1;
-  uint32_t max_iterations = 10000000;
-  uint32_t min_samples = 4;
-  uint32_t max_samples = 1000;
-  int64_t min_duration = 500 * 1000;         // 500 * 1000 nanoseconds = 500 us
-  int64_t max_duration = 1000 * 1000 * 1000; // 1e9 nanoseconds = 1 second
-  double epsilon = 0.0001;
-  double scaling_factor = 1.4;
-};
 
 class RefinableRuntimeEstimator {
   uint32_t iterations = 0;
@@ -109,38 +112,6 @@ public:
     return ratio;
   }
 };
-
-struct BenchmarkResult {
-  uint64_t total_iterations = 0;
-  double cycles = 0;
-  double standard_deviation = 0;
-  uint64_t min = UINT64_MAX;
-  uint64_t max = 0;
-};
-
-struct BenchmarkTarget {
-  using IndexedFnPtr = uint64_t (*)(uint32_t);
-  using IndexlessFnPtr = uint64_t (*)();
-
-  enum class Kind : uint8_t { Indexed, Indexless } kind;
-  union {
-    IndexedFnPtr indexed_fn_ptr;
-    IndexlessFnPtr indexless_fn_ptr;
-  };
-
-  LIBC_INLINE BenchmarkTarget(IndexedFnPtr func)
-      : kind(Kind::Indexed), indexed_fn_ptr(func) {}
-  LIBC_INLINE BenchmarkTarget(IndexlessFnPtr func)
-      : kind(Kind::Indexless), indexless_fn_ptr(func) {}
-
-  LIBC_INLINE uint64_t operator()([[maybe_unused]] uint32_t call_index) const {
-    return kind == Kind::Indexed ? indexed_fn_ptr(call_index)
-                                 : indexless_fn_ptr();
-  }
-};
-
-BenchmarkResult benchmark(const BenchmarkOptions &options,
-                          const BenchmarkTarget &target);
 
 class Benchmark {
   const BenchmarkTarget target;
